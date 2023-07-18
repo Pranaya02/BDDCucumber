7a57dd709ae86adfe1e6ad0e50f518a1f842d453
Administrator <Administrator@172.31.13.62>
2023-07-18 08:03:52 +0000

    testng

---
+++ testNGNewtest/src/main/java/ParallelScripts/SampleTwoTest.java
@@ -0,0 +1,28 @@
+package ParallelScripts;
+
+import org.testng.annotations.Test;
+
+public class SampleTwoTest {
+  
+	@Test
+	  public void testOne() {
+		  
+		long id = Thread.currentThread().getId();
+		  System.out.println("TestOne in SampleTwo..."  +id);
+		  
+	  }
+	@Test
+	  public void testTwo() {
+		  
+		long id = Thread.currentThread().getId();
+		  System.out.println("TestTwo in SampleTwo..."  +id);
+		  
+	  }
+	@Test
+	  public void testThree() {
+		  
+		long id = Thread.currentThread().getId();
+		  System.out.println("TestThree in SampleTwo..."  +id);
+		  
+	  }
+}
